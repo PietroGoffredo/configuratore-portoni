875341eeebfc507a7176402f79fb45f862855788
PietroGoffredo <git config --global user.email pietrogoffredo22@gmail.com>
2026-01-23 20:10:38 +0100

    Piccoli fix

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ export default function App() {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', overflow: 'hidden' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100dvh', overflow: 'hidden' }}>
       
       <Navbar />
 
